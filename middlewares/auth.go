1ccef63743aec9e7df46f0ac820cff038d4e6dc3
copyniinja <killfyz0.o@gmail.com>
2025-10-07 18:55:45 +0600

    fix(authorization middleware): implement safety check when no authorization header is passed

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -10,11 +10,18 @@ import (
 
 //Authorization func is a middleware that check and verify the json token and  add userid
 func Authorization(c *gin.Context) {
+
   //get token from header
   bearerToken:= c.Request.Header.Get("Authorization");
+  //checks if its empty
+  if bearerToken == "" {
+        c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+        c.Abort()
+        return
+    }
   //get the token part //Bearer <token>
   parts:=strings.Split(bearerToken, " ");
-
+  
   //verify token
   id,err:=utils.VerifyToken(parts[1]);
 
